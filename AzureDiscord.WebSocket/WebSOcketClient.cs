63590dcf576d170b5091d83d3ea208aaf9ad4435
Jordan Kelwick <jordankelwick@gmail.com>
2018-09-19 23:39:12 +0100

    Stuff

--- AzureDiscord.WebSocket/WebSOcketClient.cs
+++ AzureDiscord.WebSocket/WebSOcketClient.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -32,24 +33,31 @@ namespace AzureDiscord.WebSocket
             _tcpClient = new TcpClient();
             await _tcpClient.ConnectAsync(WebSocketUrl, Port);
             _stream = _tcpClient.GetStream();
-
-            
+            var updrageRequest = Encoding.ASCII.GetBytes(HttpUpgradeRequestString());
+            await _stream.WriteAsync(updrageRequest, 0, updrageRequest.Length);
+            using (var responseStream = new MemoryStream())
+            {
+                await _stream.CopyToAsync(responseStream);
+                responseStream.Position = 0;
+                var data = responseStream.ToArray();
+                var response = Encoding.ASCII.GetString(data);
+            }
         }
 
         private string HttpUpgradeRequestString()
         {
             var key = new byte[16];
             var rand = new Random();
-            for (var i = 0; i < 16; i++)
-            {
-                key[i] = rand.
-            }
+            
+            rand.NextBytes(key);
             var builder = new StringBuilder();
-            builder.AppendLine("GET / HTTP/1.1");
+            builder.AppendLine($"GET {WebSocketUrl} HTTP/1.1");
             builder.AppendLine($"Host: {WebSocketUrl}");
             builder.AppendLine("Upgrade: websocket");
             builder.AppendLine("Connection: Upgrade");
-            builder.AppendLine("Sec-WebSocket-Key: ")
+            builder.AppendLine($"Sec-WebSocket-Key: {Convert.ToBase64String(key)}");
+            builder.AppendLine("Sec-WebSocket-Version: 13");
+            builder.AppendLine();
             return builder.ToString();
         }
     }
---
+++ TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using AzureDiscord.WebSocket;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args) => MainAsync(args).GetAwaiter().GetResult();
+        static async Task MainAsync(string[] args)
+        {
+            using (var client = new WebSocketClient("gateway.discord.gg", 80))
+            {
+                await client.Connect();
+            }
+        }
+    }
+}
