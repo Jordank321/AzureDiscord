a2bbd92c747d307cc02faef3879a2c0218ed7d19
Jordan Kelwick <jordankelwick@gmail.com>
2018-09-19 21:36:24 +0100

    WIP

---
+++ AzureDiscord.WebSocket/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace AzureDiscord.WebSocket
+{
+    public class WebSocketClient : IDisposable
+    {
+        private TcpClient _tcpClient;
+        private NetworkStream _stream;
+
+        public WebSocketClient(string webSocketUrl, int port)
+        {
+            WebSocketUrl = webSocketUrl;
+            Port = port;
+        }
+
+        public string WebSocketUrl { get; }
+        public int Port { get; }
+
+        public void Dispose()
+        {
+            _tcpClient.Dispose();
+            _stream.Dispose();
+        }
+
+        public async Task Connect()
+        {
+            //var ip = await Dns.GetHostAddressesAsync(WebSocketUrl);
+            _tcpClient = new TcpClient();
+            await _tcpClient.ConnectAsync(WebSocketUrl, Port);
+            _stream = _tcpClient.GetStream();
+        }
+    }
+}
